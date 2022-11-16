085327b227bab0daa618141b1a5682fdf617c246
BerryDream7 <alina-kud@mail.ru>
2022-11-16 19:58:58 +0300

    add javaMiles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 3256;
+
+        int miles = price / 20;
+        System.out.println("Бонусные милли:" + miles);
+
+    }
+}
